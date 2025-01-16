465ff3d10494ad71bfc9f7451b2afb3f29d0002b
Espen Høgbakk <espen@hogbakk.no>
2025-01-16 19:59:44 +0100

    Revert "Add production token"
    This reverts commit 057a4e48d14ab653e21fb3c67219c175c4b160b1.

--- anchor-watch/map/config.js
+++ anchor-watch/map/config.js
@@ -1,7 +1,7 @@
 export const CLOUDKIT_CONFIG = {
   containerIdentifier: "iCloud.com.highhillbits.anchorwatchpro",
-  apiToken: "473e94ebaef019390e4a96151a5b4edd026ea33a99771a94022d9374aa87ff2d",
-  environment: "production",
+  apiToken: "0aaf97285b55a01cc8ecc420c330f194e58f1b88b4edacdd532f22a391f3de46",
+  environment: "development",
 };
 
 export const MAPKIT_TOKEN =

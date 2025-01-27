96a3c1264dec34e542e8d1241ac3b81c15658528
Espen Høgbakk <espen@hogbakk.no>
2025-01-27 10:23:48 +0100

    fix: Move map to live and update tokens

---
+++ anchor-watch/live/config.js
@@ -0,0 +1,11 @@
+export const CLOUDKIT_CONFIG = {
+  containerIdentifier: "iCloud.com.highhillbits.anchorwatchpro",
+  apiToken: window.development
+    ? "0aaf97285b55a01cc8ecc420c330f194e58f1b88b4edacdd532f22a391f3de46"
+    : "473e94ebaef019390e4a96151a5b4edd026ea33a99771a94022d9374aa87ff2d",
+  environment: window.development ? "development" : "production",
+};
+
+export const MAPKIT_TOKEN = window.development
+  ? "eyJraWQiOiJKQzhEUVhTN0FMIiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiIyOVY5SjZDMlEyIiwiaWF0IjoxNzM3OTY5NzQ1LCJleHAiOjE3Mzg2NTU5OTl9.yw-_eFb5cO9zobuq3W_WC7oFMky8oiplMvPUVQKD8tKxHvz6lKPLbEgFDQHyOK4hIKQ20--fkoGyYKcnjNsHGw"
+  : "eyJraWQiOiJUTjNKUDRIUDhSIiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiIyOVY5SjZDMlEyIiwiaWF0IjoxNzM3OTY5Njk2LCJvcmlnaW4iOiJoaWdoaGlsbGJpdHMuY29tIn0.chRDQ0ajr6vMPk5lEvs6FKy_5ELp2uUtU8CEqjefYExZjRlZ5k3Z5oi0mj2hrKd9NTsh2JRR0SHDUqjIFacXTw";
